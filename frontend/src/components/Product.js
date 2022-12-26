5f3f72e15f2628a4e38872da54fbc598b0f20e8b
Kobi Rozin <kobirozin911@gmail.com>
2022-12-27 00:16:25 +0200

    Part-15-Complete-Cart-Screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,36 @@
-import React from 'react';
-
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { toast } from 'react-toastify';
 import Rating from './Rating';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { Store } from '../Store';
 
 export default function Product(props) {
+  const navigate = useNavigate();
   const { product } = props;
 
- 
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      toast.error('Sorry. Product is out of stock');
+      return;
+    }
+
+    ctxDispatch({
+      type: 'CART_ADD_ITEM',
+      payload: { ...item, quantity },
+    });
+    navigate('/car');
+  };
 
   return (
     <Card>
@@ -21,9 +43,13 @@ export default function Product(props) {
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
         <Card.Text>${product.price}</Card.Text>
-        <Button >
-          Add To Cart
-        </Button>
+        {product.countInStock === 0 ? (
+          <Button variant="light" disabled>
+            Out of stock
+          </Button>
+        ) : (
+          <Button onClick={() => addToCartHandler(product)}>Add To Cart</Button>
+        )}
       </Card.Body>
     </Card>
   );
